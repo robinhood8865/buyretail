c9340af5e64e391bab6b7b386319d2f851b4ecb5
robinhood8865 <robinhood8865@gmail.com>
2023-01-09 13:34:41 +0900

    with widget

--- public/widget-embed.js
+++ public/widget-embed.js
@@ -1,6 +1,6 @@
 var cssFile = document.createElement("link");
 cssFile.rel = "stylesheet";
-cssFile.href = "./widget/index.css";
+cssFile.href = "https://custdelight-3du3.onrender.com/widget/index.css";
 document.head.appendChild(cssFile);
 
 var elem = document.createElement("div");
@@ -10,18 +10,5 @@ document.body.appendChild(elem);
 
 var jsFile1 = document.createElement("script");
 jsFile1.type = "text/javascript";
-jsFile1.src = "./widget/index.js";
+jsFile1.src = "https://custdelight-3du3.onrender.com/widget/index.js";
 document.body.appendChild(jsFile1);
-
-// var currentURL = window.location.href;
-// var splits = currentURL.split("/");
-// var domain = console.log(splits[2]);
-
-// var apiURL = "https://192.168.112.159:5000/api";
-// fetch(apiURL, {
-//   method: "POST",
-//   body: JSON.stringify({ domain }),
-//   headers: { "Content-type": "application/json; charset=UTF-8" },
-// })
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
